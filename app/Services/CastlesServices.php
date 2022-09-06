617e1368af2942631ada84b9031ae72250d36ed0
Weuller Costa <weuller.costa@novahaus.com.br>
2022-09-06 20:05:33 -0300

    Adding heroes and castle list

--- app/Services/CastlesServices.php
+++ app/Services/CastlesServices.php
@@ -3,10 +3,11 @@
 namespace App\Services;
 
 use App\Models\Castle;
+use Illuminate\Support\Facades\DB;
 
 class CastlesServices {
 
     public function castles() {
-        return Castle::orderBy('castle_name')->get();
+        return Castle::orderBy('name')->select('clan_name', 'name', DB::raw('FROM_UNIXTIME(siegeDate/1000, "%d/%m/%Y %H:%i") as siege_date'))->leftJoin('clan_data', 'id', '=', 'hasCastle')->get();
     }
 }
--- app/Services/HeroesServices.php
+++ app/Services/HeroesServices.php
@@ -5,7 +5,8 @@ namespace App\Services;
 use App\Models\Heroes;
 
 class HeroesServices {
+
     public function heroes() {
-        return Heroes::where('active', 1)->get();
+        return Heroes::select('class_list.class_name', 'characters.char_name')->leftJoin('characters', 'obj_Id', '=', 'char_id')->leftJoin('class_list', 'class_id', '=', 'id')->orderByDesc('class_list.class_name')->get();
     }
 }
